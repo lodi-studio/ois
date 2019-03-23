0da18872fa51eb5704125a57551c8c3085289179
azjhe <arjayazana25@gmail.com>
2019-03-23 13:33:15 +0800

    changed config files to properly run to my local server

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/ois/';
+$config['base_url'] = 'http://localhost:8877/ois/';
 
 /*
 |--------------------------------------------------------------------------
